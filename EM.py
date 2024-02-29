868fe1385cc60fb6822cd9c6c797339f80a87bb1
Frank Yan <frank.yan@student.unsw.edu.au>
2024-02-29 15:54:20 +1100

    change to EM.py

--- EM.py
+++ EM.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Parameters
 alpha = 0.1
-mu = 0.05
+mu = 0.0
 sigma = 0.1
 initial_condition = 0.03
 T = 1.0  
@@ -13,18 +13,18 @@ dt = T / N
 # Wiener process increments
 dW = np.random.normal(0, np.sqrt(dt), N)
 
-t_values = np.linspace(0, T, N+1)
-X_values = np.zeros(N+1)
+t = np.linspace(0, T, N+1)
+X = np.zeros(N+1)
 
-X_values[0] = initial_condition
+X[0] = initial_condition
 
 # Euler-Maruyama method
 for i in range(N):
-    drift = alpha * (mu - X_values[i])
+    drift = alpha * (mu - X[i])
     diffusion = sigma
-    X_values[i+1] = X_values[i] + drift * dt + diffusion * dW[i]
+    X[i+1] = X[i] + drift * dt + diffusion * dW[i]
 
-plt.plot(t_values, X_values, label='Vasicek Model')
+plt.plot(t, X, label='Vasicek Model')
 plt.title('Simulation of Vasicek Model using Euler-Maruyama Method')
 plt.xlabel('Time')
 plt.ylabel('Interest Rate')
